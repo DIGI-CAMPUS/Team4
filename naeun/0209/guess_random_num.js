311095aada153eafe4d059bf72ca5ccc4003f832
naaaaeun <naeunlee1223@gmail.com>
2023-02-09 18:51:28 +0900

    js

---
+++ naeun/0209/guess_random_num.js
@@ -0,0 +1,30 @@
+
+let x=Math.floor(Math.random()*100);
+alert("0~99 사이의 숫자 x가 생성되었습니다.");
+alert(x)
+
+let count=1;
+let big=99;
+let small=0;
+
+while(true){  
+alert(count+"번째 시도");
+let guess = parseInt(prompt(" 숫자를 입력하세요", small+"~"+big));
+
+if(guess<x){
+  alert("x는 더 큰 수 입니다.")
+  small=guess+1;
+}
+else if(guess>x){
+  alert("x는 더 작은 수 입니다.") 
+  big=guess-1;
+}
+else if(guess===x){
+  alert("정답입니다!"+count+"회 만에 맞췄습니다.")
+  break;
+}
+else{
+  alert("숫자를 입력하세요.")
+}
+count++;
+} 
